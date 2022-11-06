8ed284d94c0b81a6d487fb831d92baf56e7d9861
naoki.sugai <musen1485@gmail.com>
2022-11-06 12:21:11 +0000

    MOdification kadai

--- mentor.rb
+++ mentor.rb
@@ -17,9 +17,9 @@ class RailsMentor < Mentor
   end
 end
 
-mentor_kirameki = Mentor.new('煌木')
-mentor_akaide =RailsMentor.new('赤出')
+kirameki = Mentor.new('煌木')
+akaide =RailsMentor.new('赤出')
 
-mentor_kirameki.job
-mentor_akaide.job
+kirameki.job
+akaide.job
 
